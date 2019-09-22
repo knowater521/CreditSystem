9c55e8f2262d3cfe3cffadd25c3df5b2c20278bf
AoliShen <55521355+AoliShen@users.noreply.github.com>
2019-09-22 21:24:37 +0800

    Revert "the first time to add all files"
    This reverts commit a5180ac8cdf29efba2df5e261a1e6a26847ad162.

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BlogsPost
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title','timestamp')
-admin.site.register(BlogsPost,BlogPostAdmin)
-
--- blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class BlogsPost(models.Model):
-    title = models.CharField(max_length = 150)
-    body = models.TextField()
-    timestamp = models.DateTimeField()
